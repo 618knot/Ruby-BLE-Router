292212be1dc88ec1fb03c572f562b0313efba626
618knot <publicac.clt618knot@gmail.com>
2025-01-14 17:25:28 +0900

    next_ip変更

--- router_example.rb
+++ router_example.rb
@@ -3,9 +3,9 @@ require_relative "lib/ruby-router/ruby-router"
   RubyRouter::Router.new(
   "eth0",
   "wlan0",
-  "192.168.10.121",
+  "172.20.0.1",
   "hci0",
   [
     "2C:CF:67:83:EA:E4",
   ]
-  ).run
+).run
