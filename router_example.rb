5442bca53c9213e2e84d47104f96580805f8b1a8
618knot <publicac.clt618knot@gmail.com>
2025-01-20 12:55:07 +0900

    nextip変更

--- router_example.rb
+++ router_example.rb
@@ -3,9 +3,13 @@ require_relative "lib/ruby-router/ruby-router"
   RubyRouter::Router.new(
   "eth0",
   "wlan0",
-  "172.20.0.1",
+  "192.168.11.1",
   "hci0",
   [
     "2C:CF:67:83:EA:E4",
   ]
 ).run
+
+sleep
+
+# sudo env GEM_PATH=$(gem env gemdir) ruby router_example.rb 
